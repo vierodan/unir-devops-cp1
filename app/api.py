e2f36c302aadfb7b25a1fcfa915164f8d2b61cd7
vierodan <vierodan@gmail.com>
2024-05-03 14:44:11 +0200

    api.py amended

--- app/api.py
+++ app/api.py
@@ -48,7 +48,7 @@ def divide(op_1, op_2):
     try:
         num_1, num_2 = util.convert_to_number(op_1), util.convert_to_number(op_2)
         if num_2 == 0:
-            raise ZeroDivisionError("Cannot divide by zero")
+            raise TypeError("Division by zero is not possible")
         return ("{}".format(CALCULATOR.divide(num_1, num_2)), http.client.OK, HEADERS)
-    except (TypeError, ZeroDivisionError) as e:
+    except TypeError as e:
         return (str(e), http.client.BAD_REQUEST, HEADERS)
